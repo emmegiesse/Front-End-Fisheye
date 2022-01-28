bb34970b697190b39a87a14ef365c15fd33cc48a
emmegiesse <martagiulia.scoditti@gmail.com>
2022-01-28 09:15:01 +0100

    20220128

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,10 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const queryString = window.location.search
+const urlparams = new URLSearchParams (queryString)
+const id = urlparams.get ('id')
+const photographer = data.photographers.find ((el)=>el.id == id)
+
+const photographersSection = document.querySelector(".photographer_section");
+const photographerModel = photographerFactory(photographer);
+const userCardDOM = photographerModel.getUserCardDOM();
+photographersSection.appendChild(userCardDOM);
