1ed03de5897d85fba06b840e99822ca9ed133e69
emmegiesse <martagiulia.scoditti@gmail.com>
2022-02-18 10:02:13 +0100

    import gallery

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,18 +1,37 @@
-function mediaFactory(data) {
-    const { title, image, likes, date, id, price } = data ;
+function mediaFactory(data, photographerId) {
+    const { title, image, video, likes, date, id, price } = data ;
+    const isVideo = video != null;
 
+	function getMediaCardDOM() {
+		const article = document.createElement( 'article' );
+        let media;
+        if (isVideo) {
+            const videoURL = `assets/images/${photographerId}/${video}` ;
+            media = document.createElement( 'video' );
+            media.setAttribute("src", videoURL);
+        }
+        else {
+            const imageURL = `assets/images/${photographerId}/${image}` ;
+            media = document.createElement( 'img' );
+            media.setAttribute("src", imageURL);
+        }
+        media.setAttribute("alt", title);
+        media.setAttribute("width", "200px");
+		const pTitle = document.createElement( 'p' );
+        pTitle.textContent = title;
+        pTitle.className = 'pTitle';
+		const pLikes = document.createElement( 'p' );
+        pLikes.textContent = likes;
+        pLikes.className = 'pLikes';
+        const pHeart = document.createElement('button');                                                                                                      
+        article.appendChild(media);
+		article.appendChild(pTitle);
+		article.appendChild(pLikes);
+        article.appendChild(pHeart);
 
+        return (article);
+    }
 
+	return { getMediaCardDOM }
 
-
-
-    "media": [
-		{
-			"id": 342550,
-			"photographerId": 82,
-			"title": "Fashion Yellow Beach",
-			"image": "Fashion_Yellow_Beach.jpg",
-			"likes": 62,
-			"date": "2011-12-08",
-			"price": 55
-		},
+}
